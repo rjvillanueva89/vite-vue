b2182fa52da2a68ef45ccf66034077ee725b1fc7
RJ Villanueva <rj.villanueva89@gmail.com>
2023-07-20 19:55:32 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue"
+import NavBar from "./components/NavBar.vue"
 </script>
 
 <template>
+  <NavBar msg="Vite + Vue" />
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
